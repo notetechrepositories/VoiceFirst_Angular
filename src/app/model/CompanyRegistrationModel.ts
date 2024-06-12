1721a0fa17e626ce695f5c7add33a5403be40d76
notetechrepositories <notetechrepositories>
2024-06-12 17:55:49 +0530

    UPDATE-COMPANY REG

---
+++ src/app/model/CompanyRegistrationModel.ts
@@ -0,0 +1,15 @@
+export class CompanyRegistrationModel {
+    company_name?: string;
+    user_name?: string;
+    address?: string;
+    country?: string;
+    region?: string;
+    sub_region?: string;
+    place?: string;
+    latitude?: number;
+    longitude?: number;
+    pincode?: string;
+    email?: string;
+    phone_number?: string;
+    password?: string;
+}
--- src/app/superadmin-companydetails/superadmin-companydetails.component.ts
+++ src/app/superadmin-companydetails/superadmin-companydetails.component.ts
@@ -11,7 +11,10 @@ import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClient, HttpClientModule } from '@angular/common/http'; 
+import { CompanyRegistrationModel } from '../model/CompanyRegistrationModel';
+import { log } from 'console';
+
 
 
 
@@ -27,15 +30,31 @@ export class SuperadminCompanydetailsComponent {
 
 
   company!: CompanyModel[];
+  companyRegistration: CompanyRegistrationModel = new CompanyRegistrationModel()
 
   companyListView:boolean=true;
   AddCompanyvisible:boolean=false;
 
-  constructor(private companyService: CompanyService, private messageService: MessageService) {}
+  searchValue: string | undefined;
+
+  pincode!: string;
+  data: any;
+  countries: any[] = [];
+  countryViewArray: any[] = [];
+  regionView!: string;
+  subRegionView!: string;
+  placeView: any[] = [];
+
+
+
+
+  constructor(private companyService: CompanyService, private http: HttpClient) {}
 
   ngOnInit() {
     this.getCompanyDetails();
+    this. fetchCountries();
   }
+
 
   getCompanyDetails(){
     this.companyService.getCompantDetails().subscribe({
@@ -53,8 +72,10 @@ export class SuperadminCompanydetailsComponent {
     return status === 1 ? 'APPROVED' : 'UNAPPROVED';
   }
 
-  getSeverityisApproved(status: number) {
-    return status === 1 ? 'success' : 'warning';
+  getSeverityisApproved(status: string) {
+    return status === "APPROVED" ? 'success' : 
+       status === "DECLINED" ? 'danger' : 
+       'warning';
   }
 
   getSeverityisActive(status: number) {
@@ -70,6 +91,125 @@ export class SuperadminCompanydetailsComponent {
   }
   onCompanyback(){
     this.companyListView=true;
+  }
+
+// -------------------------------------------
+
+
+  fetchCountries() {
+    this.http.get<any[]>('/assets/countryList.json').subscribe({
+      next: (data) => {
+        this.countries = data;
+      },
+      error: (error) => {
+        console.error('There was an error fetching the JSON data!', error);
+      },
+    });
+  }
+
+  getCountryDetailsByPincode(event:any) {
+    this.pincode=event.target.value;
+    console.log(this.pincode);
+    this.companyRegistration.country='';
+    this.companyRegistration.region='';
+    this.companyRegistration.sub_region='';
+    this.companyRegistration.place='';
+    this.countryViewArray = [];
+    this.placeView = [];
+    this.regionView = '';
+    this.subRegionView = '';
+    
+    const apiUrl = `http://api.geonames.org/postalCodeLookupJSON?postalcode=${this.pincode}&username=anil`;
+   
+    this.http.get<any>(apiUrl).subscribe({
+      next: (response) => {
+        this.data = response;
+        console.log(this.data);
+        
+        var filterData=[];
+        console.log(this.data);
+        for (let j = 0; j < this.data.postalcodes.length; j++) {
+          let array = this.data.postalcodes[j].postalcode.split(" ");
+          let compressedString = array.join("");
+          if(compressedString==this.pincode){
+            filterData.push(this.data.postalcodes[j])
+          }
+        }
+        this.data.postalcodes=filterData;
+        console.log(this.data);
+        let seenCountryCodes = new Set();
+        for (let i = 0; i < this.data.postalcodes.length; i++) {
+          let countryCode = this.data.postalcodes[i].countryCode;
+          if (!seenCountryCodes.has(countryCode)) {
+            seenCountryCodes.add(countryCode);
+            let country = this.countries.find(
+              (c) => c.countryCode === countryCode
+            );
+            if (country) {
+              this.countryViewArray.push(country);
+              console.log(this.countryViewArray);
+              
+            }
+          }
+        }
+        if (this.countryViewArray.length == 1) {
+          this.companyRegistration.country=this.countryViewArray[0].countryName;
+          this.companyRegistration.region = this.data.postalcodes[0].adminName1;
+          this.companyRegistration.sub_region = this.data.postalcodes[0].adminName2;
+          this.companyRegistration.place = this.data.postalcodes[0].placeName;
+        }
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+    });
+  }
+
+  changeCountry(event: any) {
+    console.log(event.target.value);
+    this.companyRegistration.place='';
+    this.placeView = [];
+    this.regionView = '';
+    this.subRegionView = '';
+    this.placeView = this.data.postalcodes.filter(
+      (country: any) => country.countryCode === event.target.value
+    );
+    
+    if(this.placeView.length==1){
+      this.companyRegistration.place=this.placeView[0].placeName;
+    }
+   
+    
+    if (this.placeView[0].adminName1 != '') {
+      this.regionView = this.placeView[0].adminName1;
+      this.companyRegistration.region=this.regionView;
+      
+    }
+    if (this.placeView[0].adminName2 != '') {
+      this.subRegionView = this.placeView[0].adminName2;
+      this.companyRegistration.sub_region=this.subRegionView;
+    }
+  }
+
+  onRegister(){
+    let country = this.countries.find(
+      (c) => c.countryCode === this.companyRegistration.country
+    );
+    console.log(this.companyRegistration);
+    
+    // this.companyRegistration.country=country.countryName;
+     
+    this.companyService.registerCompany(this.companyRegistration).subscribe({
+      next:(res)=>{
+        console.log(res);
+        
+      },
+      error:(error)=>{
+        console.log(error);
+        
+      }
+    })
+    
   }
 }
 
