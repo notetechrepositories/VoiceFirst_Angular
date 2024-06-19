0f0a7a13942654cba23aae285e9d9fda9a49c72f
notetechrepositories <notetechrepositories>
2024-06-19 14:27:19 +0530

    update-19-6-24

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -8,7 +8,7 @@ export class CompanyService {
 
   constructor(private http:HttpClient){}
 
-  accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKV1RTZXJ2aWNlQWNjZXNzVG9rZW4iLCJqdGkiOiI2MjhmNzcyYS01MTUyLTQwZWMtYjE0ZC1kMTE5YTBkNmQ4MzgiLCJpYXQiOiIxMi0wNi0yMDI0IDEyOjA2OjMxIiwidXNlcl9pZCI6IjEiLCJleHAiOjE3MjE2NDk5OTEsImlzcyI6IkpXVEF1dGhlbnRpY2F0aW9uU2VydmVyIiwiYXVkIjoiSldUU2VydmljZVBvc3RtYW5DbGllbnQifQ.xciG0bcvgutvpwtT8uX8tPW01S-JoohDFkzAVchtHfA"
+  accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKV1RTZXJ2aWNlQWNjZXNzVG9rZW4iLCJqdGkiOiJjMzdiNmY4ZS0zZTAwLTQwMzktYjAzYi00MDIzNzllOTE4MmEiLCJpYXQiOiIxNC0wNi0yMDI0IDEwOjE0OjMwIiwidXNlcl9pZCI6IjEiLCJleHAiOjE3MjE4MTYwNzAsImlzcyI6IkpXVEF1dGhlbnRpY2F0aW9uU2VydmVyIiwiYXVkIjoiSldUU2VydmljZVBvc3RtYW5DbGllbnQifQ.TORSKJB5fdcmWPgDUc8UN8XqR_j4wxYRquhkC4HH8pM"
   headers = new HttpHeaders({
     'Authorization': `Bearer ${this.accessToken}`
   });
@@ -17,8 +17,20 @@ getCompantDetails(){
   return this.http.get<any>("https://localhost:7134/api/company/get-company-details");
 }
 
-registerCompany(data:any){
+registerCompanyByAdmin(data:any){
   return this.http.post<any>(`https://localhost:7134/api/company/register-company-by-admin`,data,{headers:this.headers})
+}
+
+registerCompanyByCompany(data:any){
+  return this.http.post<any>(`https://localhost:7134/api/company/register-company-by-company`,data)
+}
+
+getBranchwithCompanyId(id:number){
+  return this.http.get<any>(`https://localhost:7134/api/branch/get-branch-with-company-by-id?companyId=${id}`,{headers:this.headers})
+}
+
+updateStatusBySuperadmin(id:number,status:number){
+  return this.http.put<any>(`https://localhost:7134/api/company/update-company-status?companyId=${id}&status=${status}`,{},{headers:this.headers})
 }
 
 }
--- src/app/sidebar-navigation/sidebar-navigation.component.ts
+++ src/app/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CompanyDashboardComponent } from "../company-dashboard/company-dashboard.component";
 import { SuperadminCompanydetailsComponent } from "../superadmin-companydetails/superadmin-companydetails.component";
+import { SuperadminDashboardComponent } from "../superadmin-dashboard/superadmin-dashboard.component";
 
 @Component({
     selector: 'app-sidebar-navigation',
     standalone: true,
     templateUrl: './sidebar-navigation.component.html',
     styleUrl: './sidebar-navigation.component.css',
-    imports: [CompanyDashboardComponent, SuperadminCompanydetailsComponent]
+    imports: [CompanyDashboardComponent, SuperadminCompanydetailsComponent, SuperadminDashboardComponent]
 })
 export class SidebarNavigationComponent {
 
